dab712a186268abef6372231304c7b43661389e8
Dan Rusei <dan.rusei@gmail.com>
2019-04-22 15:32:35 +0300

    routeguide first part

---
+++ GRPC/grpc_routeguide/README.md
@@ -0,0 +1,3 @@
+Tutorial provides a basic Go programmer’s introduction to working with gRPC
+
+https://grpc.io/docs/tutorials/basic/go.html
---
+++ GRPC/grpc_routeguide/server/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+
+	pb "github.com/Danr17/GO_coding/GRPC/grpc_routeguide/routeguide"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
+
+type routeGuideServer struct {
+	savedFeatures []*pb.Feature //read- only after initialized
+	mu            sync.Mutex    // protects routeNotes
+	routeNotes    map[string][]*pb.RouteNote
+}
+
+//GetFeature returns the fature at the given point.
+func (s *routeGuideServer) GetGeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	//no feature was found, return an unamed feature
+	return &pb.Feature{Location: point}, nil
+}
+
+// ListFeatures lists all features contained within the given bounding Rectangle.
+func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListfeaturesServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// RecordRoute records a route composited of a sequence of points.
+//
+// It gets a stream of points, and responds with statistics about the "trip":
+// number of points,  number of known features visited, total distance traveled, and
+// total time spent.
+func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
+	var pointCount, featureCount, distance int32
+	var lastPoint *pb.Point
+	startTime := time.Now()
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			endTime := time.Now()
+			return stream.SendAndClose(&pb.RouteSummary{
+				PointCount:   pointCount,
+				FeatureCount: featureCount,
+				Distance:     distance,
+				ElapsedTime:  int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
