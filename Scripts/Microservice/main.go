d1de104cddc714631047f16a29d728c76a2d5939
Dan Rusei <dan.rusei@gmail.com>
2019-04-23 16:58:04 +0300

    microservice script final build

--- Scripts/Microservice/main.go
+++ Scripts/Microservice/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -10,12 +11,13 @@ import (
 )
 
 var (
-	GcukCertFile    = os.Getenv("GCUK_CERT_FILE")
-	GcukKeyFile     = os.Getenv("GCUK_KEY_FILE")
-	GcukServiceAddr = os.Getenv("GCUK_SERVICE_ADDR")
+	GcukCertFile    = flag.String("GCUK_CERT_FILE", "certs/local.localhost.cert", "The TLS cert file")
+	GcukKeyFile     = flag.String("GCUK_KEY_FILE", "certs/local.localhost.key", "The TLS key file")
+	GcukServiceAddr = flag.String("GCUK_SERVICE_ADDR", ":8080", "The server port")
 )
 
 func main() {
+	flag.Parse()
 	logger := log.New(os.Stdout, "gcuk ", log.LstdFlags|log.Lshortfile)
 
 	h := homepage.NewHandlers(logger)
@@ -23,10 +25,10 @@ func main() {
 	mux := http.NewServeMux()
 	h.SetupRoutes(mux)
 
-	srv := server.New(mux, GcukServiceAddr)
+	srv := server.New(mux, *GcukServiceAddr)
 
 	logger.Println("server starting")
-	err := srv.ListenAndServeTLS(GcukCertFile, GcukKeyFile)
+	err := srv.ListenAndServeTLS(*GcukCertFile, *GcukKeyFile)
 	if err != nil {
 		logger.Fatalf("server failed to start: %v", err)
 	}
