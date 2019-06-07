f31ffd29bef7cbc799e8c3f3d0c023908aaeb5b7
Dan Rusei <dan.rusei@gmail.com>
2019-06-07 12:45:20 +0300

    stringsvc1 complete

--- GO-Kit/stringsvc/main.go
+++
@@ -1,8 +0,0 @@
-
-import "context"
-
-//StringService provides operations on strings
-type StringService interface{
-	Uppercase(string) (string, error)
-	Count(string) int
-}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Danr17/GO_coding
 
 go 1.12
+
+require (
+	github.com/go-kit/kit v0.8.0
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+)
