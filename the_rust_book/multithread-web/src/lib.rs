bf7b051feb19a59c7b2c6de32f995a8e3dddb136
Dan Rusei <dan.rusei@gmail.com>
2020-04-20 14:33:35 +0300

    Signaling to the Threads to Stop Listening for Jobs

--- the_rust_book/multithread-web/src/lib.rs
+++ the_rust_book/multithread-web/src/lib.rs
@@ -3,12 +3,17 @@ use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
 
-struct Job;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -18,7 +23,6 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -30,8 +34,8 @@ impl ThreadPool {
 
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
-
         }
+
         ThreadPool {
             workers,
             sender,
@@ -39,27 +43,66 @@ impl ThreadPool {
     }
 
     pub fn execute<F>(&self, f: F)
-        where 
+        where
             F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn( || {
-            receiver
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) ->
+        Worker {
+
+        let thread = thread::spawn(move ||{
+            loop {
+                let message = receiver.lock().unwrap().recv().unwrap();
+
+                match message {
+                    Message::NewJob(job) => {
+                        println!("Worker {} got a job; executing.", id);
+
+                        job();
+                    },
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+
+                        break;
+                    },
+                }
+            }
         });
 
         Worker {
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
