f65f72daadcc6a29bafba96b47729390707b5708
Dan Rusei <dan.rusei@gmail.com>
2020-04-27 19:53:28 +0300

    adding the image

--- test_flutter_basics/lib/main.dart
+++ test_flutter_basics/lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text('Oppss hopaaa did '),
-      )
+        title: Text('Test out'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/flutter.png'),
+        ),
+      ),
     ),
   ),
   );
